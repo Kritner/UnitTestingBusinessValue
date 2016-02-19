cffd7db6101224062e738414bf10df374769a8cd
Russ Hammett <Kritner@gmail.com>
2016-02-18 20:10:02 -0500

    Initial project creation

---
+++ UnitTestingBusinessValue.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestingBusinessValue.ConsoleApp
+{
+
+    /// <summary>
+    /// Entry point into application
+    /// </summary>
+    class Program
+    {
+
+        /// <summary>
+        /// Entry point
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
